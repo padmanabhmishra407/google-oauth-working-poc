f2288f981d6fa7c2b785ec06e3534c4f036ee5c5
Padmanabh Mishra <padmanabhmishra@Padmanabhs-MacBook-Air.local>
2024-02-08 12:39:34 +0530

    Final POC

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import { GoogleLogin } from "@react-oauth/google";
+import { googleLogout } from "@react-oauth/google";
+import { useState } from "react";
+import { jwtDecode } from "jwt-decode";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [decoded, setDecoded] = useState({});
+
+  const decode_store_jwt = (token) => {
+    const decode = jwtDecode(token);
+    console.log(decode, ">>>>>");
+    setDecoded(decode);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            minHeight: "100vh",
+            height: "100%",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {!isLoggedIn ? (
+            <GoogleLogin
+              onSuccess={(credentialResponse) => {
+                decode_store_jwt(credentialResponse.credential);
+                console.log(credentialResponse)
+                setIsLoggedIn(true);
+              }}
+              onError={() => {
+                console.log("Login Failed");
+              }}
+              useOneTap
+            />
+          ) : (
+            // <button onClick={() => googleLogout()}>Logout</button>
+            googleLogout()
+          )}
+        </div>
+        <div>
+          <h2>Decoded Object:</h2>
+          <ul>
+            {Object.entries(decoded).map(([key, value]) => (
+              <li key={key}>
+                <strong>{key}:</strong> {value}
+              </li>
+            ))}
+          </ul>
+        </div>
     </div>
   );
 }
